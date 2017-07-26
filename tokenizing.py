bce910c3bb3a1406cad398cebb92fc39a2665851
yaniv <yanivshachor63@gmail.com>
2017-07-26 12:09:08 +0300

    start of nlp sentiment project

---
+++ tokenizing.py
@@ -0,0 +1,65 @@
+import re 
+
+# The components of the tokenizer:
+regex_strings = (
+    # Phone numbers:
+    r"""
+    (?:
+      (?:            # (international)
+        \+?[01]
+        [\-\s.]*
+      )?            
+      (?:            # (area code)
+        [\(]?
+        \d{3}
+        [\-\s.\)]*
+      )?    
+      \d{3}          # exchange
+      [\-\s.]*   
+      \d{4}          # base
+    )"""
+    ,
+    # Remaining word types:
+    r"""
+    (?:[a-z][a-z'\-_]+[a-z])       # Words with apostrophes or dashes.
+    |
+    (?:[+\-]?\d+[,/.:-]\d+[+\-]?)  # Numbers, including fractions, decimals.
+    |
+    (?:[\w_]+)                     # Words without apostrophes or dashes.
+    |
+    (?:\.(?:\s*\.){1,})            # Ellipsis dots. 
+    |
+    (?:\S)                         # Everything else that isn't whitespace.
+    """
+    )
+
+######################################################################
+# This is the core tokenizing regex:
+    
+word_re = re.compile(r"""(%s)""" % "|".join(regex_strings), re.VERBOSE | re.I | re.UNICODE)
+
+amp = "&amp;"
+
+######################################################################
+
+class Tokenizer:
+    def __init__(self, preserve_case=False):
+        self.preserve_case = preserve_case
+
+    def tokenize(self, s):
+        """
+        Argument: s -- any string or unicode object
+        Value: a tokenize list of strings; conatenating this list returns the original string if preserve_case=False
+        """        
+        # Try to ensure unicode:
+        try:
+            s = unicode(s)
+        except UnicodeDecodeError:
+            s = str(s).encode('string_escape')
+            s = unicode(s)
+        # Tokenize:
+        words = word_re.findall(s)
+        # Possible alter the case, but avoid changing emoticons like :D into :d:
+        if not self.preserve_case:            
+            words = map((lambda x : x.lower()), words)
+        return words
